5cb440a384fad85559abba2e7217aac482af6342
Lynda-FEDDAK <feddaklynda@gmail.com>
2023-12-23 13:52:03 +0100

    update queries

--- Queries.sql
+++ Queries.sql
@@ -2,18 +2,29 @@
 use accidentsroutiers;
 -- Query 1: Retrieve information about accidents involving pedestrians, including their characteristics and the type of vehicle involved
 
+INSERT INTO accidentsroutiers.categorie_du_vehicule
+VALUES
+('-1',
+'unconnue');
+
 SELECT 
     usagers.id_usager,
-    usagers.catu AS 'User Category',
-    usagers.grav AS 'Severity',
+    gravite.gravite AS 'Severity',
     caracteristiques.mois AS 'Month',
-    vehicules.catv AS 'Vehicle Category'
+    categorie_du_vehicule.categorie_du_vehicule AS 'Vehicle Category'
 FROM 
     usagers
 JOIN 
-    caracteristiques
+    caracteristiques on ( Accident_Id = Num_Acc)
 JOIN 
     vehicules
+using ( Num_Acc)
+JOIN
+	categorie_du_vehicule on ( vehicules.catv = categorie_du_vehicule.id_categorie_du_vehicule)
+JOIN
+	gravite on ( usagers.grav = gravite.id_gravite)
+JOIN 
+	categorie_usager on ( usagers.catu = categorie_usager.id_categorie_usager)
 WHERE 
     usagers.catu = 3;
 
@@ -25,24 +36,30 @@ SELECT
 FROM 
     conditions_atmospheriques
 JOIN 
-    caracteristiques ON conditions_atmospheriques.id_conditions_atmospheriques = caracteristiques.atm
+	caracteristiques ON conditions_atmospheriques.id_conditions_atmospheriques = caracteristiques.atm
 GROUP BY 
     conditions_atmospheriques.conditions_atmospheriques
 ORDER BY 
     COUNT(*) DESC
-LIMIT 1;
+LIMIT 5;
 
 
--- Query 3: Find the average number of vehicles involved in accidents for each category of the road.
-SELECT 
-    categorie_de_route.categorie_de_route,
-    AVG(lieux.nbv) AS 'Average Vehicles'
-FROM 
-    lieux
-JOIN 
-    categorie_de_route ON lieux.catr = categorie_de_route.id_categorie_de_route
-GROUP BY 
-    categorie_de_route.categorie_de_route;
+-- Query 3: Find the average number of accidents for each category of the road.
+SELECT
+    cat AS "Categorie De Route",
+    c_acc / (SELECT COUNT(*) FROM categorie_de_route) AS "Avg Num Accidents"
+FROM (
+    SELECT
+        categorie_de_route.categorie_de_route AS cat,
+        COUNT(*) AS c_acc
+    FROM
+        lieux
+    JOIN
+        categorie_de_route ON categorie_de_route.id_categorie_de_route = catr
+    GROUP BY
+        catr
+) AS T;
+
 -- Query 4: Identify the top 5 locations (lieux) with the highest number of accidents.
 
 SELECT 
@@ -56,8 +73,7 @@ ORDER BY
     COUNT(*) DESC
 LIMIT 5;
 
-
--- Query 6: List the different types of motorizations (type_motorisation) and the count of accidents for each type.
+-- Query 5: List the different types of motorizations (type_motorisation) and the count of accidents for each type.
 SELECT 
     type_motorisation.type_motorisation,
     COUNT(*) AS 'Accident Count'
@@ -67,5 +83,4 @@ JOIN
     vehicules ON type_motorisation.id_type_motorisation = vehicules.motor
 GROUP BY 
     type_motorisation.type_motorisation;
-
 
